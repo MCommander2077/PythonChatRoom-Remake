4eb66dccf707ac6fff5c313b130ba4ce61a44e61
MCommander2077 <2679935111@qq.com>
2023-02-09 21:55:58 +0800

    v2 update

--- v2/client.py
+++ v2/client.py
@@ -1,11 +1,66 @@
+import customtkinter as ctk
+import tkinter as tk
 import socket
 import threading
 import time
+import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(('127.0.0.1', 8001))
-time.sleep(2)
-sock.send('P^$$VV0rd'.encode())
-print(sock.recv(1024).decode('utf-8'))
-sock.close()
-input()
+Bool_Is_Connect = False
+Str_Send_Message = ""
+Str_Data_Receive = ""
+
+sock_ip = "127.0.0.1"
+sock_port = 50583
+
+verification_KEY = '''ex|ZhXd8;$X}oNqTo4{$Clq;hy6ums&'cye"q^9TGI3U/=e@pwLMB&c#p/B(34jZDffrkiOwNa3dCf2ZWnoBF7$\=N&zmoeum8#'''
+USERNAME = socket.gethostname()
+# 密码功能正在火速研发中()
+# PASSWORD = ""
+
+
+class Client:
+    def __init__(self):
+        # ipv4  TCP
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self, server_ip, server_port):
+        self.client.connect((server_ip, server_port))
+        # 验证
+        self.client.send(verification_KEY.encode())
+        if self.client.recv(1024).decode('utf-8') == 'False':
+            self.client.close()
+            sys.exit("KEY VERIFICATION FAILED")
+
+    def send(self):
+        while Bool_Is_Connect:
+            message = input("message:")
+            self.client.send(f'{USERNAME}:{message}'.encode())
+
+    def receive(self):
+        while Bool_Is_Connect:
+            response = self.client.recv(1024).decode('utf-8')
+            print(response)
+
+    def disconnect(self):
+        self.client.close()
+
+
+client = Client()
+client.connect('127.0.0.1', 50583)
+
+thread_send = threading.Thread(target=client.send)
+thread_receive = threading.Thread(target=client.receive)
+
+thread_send.start()
+thread_receive.start()
+
+
+root = ctk.CTk()
+root.title("TCP Client")
+root.geometry("300x300")
+root.resizable(False, False)
+root.config(bg="pink")
+root.columnconfigure(0, weight=1)
+root.mainloop()
+
+client.disconnect()
--- v2/server.py
+++ v2/server.py
@@ -1,19 +1,87 @@
-import socket
+from socketserver import BaseRequestHandler, ThreadingTCPServer
+
 import threading
+import socket
+import time
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind(('0.0.0.0', 8001))
-sock.listen(5)
+# 日志显示
 
-while True:
-        connection,address = sock.accept()
-        try:
-            connection.settimeout(5)
-            buf = connection.recv(1024).decode('utf-8')
-            if buf == 'P^$$VV0rd':
-                connection.send('welcome to server'.encode('utf-8'))
-            else:
-                connection.send('please go out!'.encode('utf-8'))
-        except socket.timeout:
-            print('connection time out')
-        connection.close()
+
+def showinfo(message):
+    if __name__ == '__main__':
+        global timer
+        print(f'[{timer} ms/info]{message}')
+
+
+def showerror(message):
+    if __name__ == '__main__':
+        global timer
+        print(f'[{timer} ms/error]{message}')
+
+
+def showwarning(message):
+    if __name__ == '__main__':
+        global timer
+        print(f'[{timer} ms/warning]{message}')
+
+
+def setup():
+    global timer
+    global sock_port
+    global verification_KEY
+    # 初始化
+    verification_KEY = '''ex|ZhXd8;$X}oNqTo4{$Clq;hy6ums&'cye"q^9TGI3U/=e@pwLMB&c#p/B(34jZDffrkiOwNa3dCf2ZWnoBF7$\=N&zmoeum8#'''
+    timer = 1
+    sock_port = 50583
+# 计时器
+
+
+def timer_loop():
+    global timer
+    while True:
+        time.sleep(0.001)
+        timer = timer + 1
+
+
+class Handler(BaseRequestHandler):
+
+    def handle(self) -> None:
+        address, pid = self.client_address
+        showinfo(f'{address} connected,wait for KEY verification.')
+
+        password_verification = self.request.recv(1024).decode('utf-8')
+        if password_verification == verification_KEY:
+            self.request.send('True'.encode('utf-8'))
+            showinfo(f'{address} KEY verification succuss,begin serving.')
+        else:
+            self.request.send('False'.encode('utf-8'))
+            showinfo(f'{address} KEY verification FAILED.disconnect.')
+            return False
+
+        while True:
+            data = self.request.recv(1024).decode('utf-8')
+            if len(data) <= -1:
+                showwarning(f'{address} disconnected!')
+                break
+            showinfo(f'user {address} send data:" {data} "')
+            self.request.sendall(f'{data}'.encode())
+        showinfo(f'{address} disconnected')
+
+
+def serve():
+    server = ThreadingTCPServer(('0.0.0.0', sock_port), Handler)
+    showinfo(f'Listening on 0.0.0.0:{sock_port}')
+    server.serve_forever()
+
+
+def main():
+    # 计时器循环线程
+    thread_timer_loop = threading.Thread(target=timer_loop)
+    thread_timer_loop.start()
+    setup()
+    serve()
+    showerror("Server is Down.")
+
+
+if __name__ == '__main__':
+    main()
